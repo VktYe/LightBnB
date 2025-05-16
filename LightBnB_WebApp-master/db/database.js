73733a951c68e3c3b09f9bef9bdc55e449ce672d
VktYe <v.yehorenko@gmail.com>
2025-05-16 01:25:20 -0300

    Install pg

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,12 @@
 const { Pool } = require("pg");
 
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "lightbnb",
+});
+
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
